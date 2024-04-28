1eefd4bce024f94fd64a942a262ea0728b4a591b
Austin Monde <austinmonde7@gmail.com>
2024-04-28 17:23:13 +0200

    added more questions to the quiz

--- game.py
+++ game.py
@@ -56,5 +56,40 @@ if question == "read only memory":
 else:
     print("Incorrect!")
 
+question = input("What does CLI stand for? ").lower()
+print(question)
+if question == "command line interface":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+question = input("What does VPN stand for? ").lower()
+print(question)
+if question == "virtual private network":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+question = input("What does GUI stand for? ").lower()
+print(question)
+if question == "graphical user interface":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+question = input("What does HTTP stand for? ").lower()
+print(question)
+if question == "hypertext transfer protocol":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score / 6) * 100) + "%.")
+
+# Prompt the user to press Enter to exit
+input("Press Enter to exit...")
