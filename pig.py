cdd4efe5187754b71f6fed71e1de5b11ec53046d
Austin Monde <austinmonde7@gmail.com>
2024-05-06 05:46:14 +0200

    Completed pig game

--- pig.py
+++ pig.py
@@ -22,6 +22,35 @@ while True:
         print("Invalid, try again.")
 
 print(players)
-input("Press enter to exit.....")
 
-    
+max_score = 50
+player_scores = [0 for _ in range(players)] #every players score will begin from 0
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "your turn has started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll != "y":
+                break
+
+            value = roll()
+            
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+        player_scores[player_idx] += current_score
+        print("Your  total score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of:", max_score)
