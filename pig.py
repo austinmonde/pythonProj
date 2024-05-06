fde497eafddf903001852cc314c759c1dc157b22
Austin Monde <austinmonde7@gmail.com>
2024-05-06 04:53:51 +0200

    initial commit

--- pig.py
+++ pig.py
@@ -7,6 +7,20 @@ def roll():
 
     return roll_result
 
-value = roll()
-print(value)
+value = roll() #call the function and add it to variable value.
+#print(value) #prints the result
 
+while True:
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit(): #checks if the input provided by the user consists only of digits
+        players = int(players)
+        if 2 <= players <= 4: #checks if the number of players entered is between 2 and 4
+            break
+        else:
+            print("Must be between 2 - 4 players")
+    else:
+        print("Invalid, try again.")
+        
+print(players)
+
+    
