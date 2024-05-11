67f2fc417010dc335df35e1d370d3d7bbc686bfe
Austin Monde <austinmonde7@gmail.com>
2024-05-11 02:00:09 +0200

    moved txt file to main directory

--- mad libs/madlib.py
+++ mad libs/madlib.py
@@ -1,3 +1,3 @@
-with open("story.txt", "r") as f: #open function allows users to open a file in "read mode"
-    story = f.read() #gives all the text in the file
+with open("story.txt", "r") as f: 
+    story = f.read() 
 print(story)
