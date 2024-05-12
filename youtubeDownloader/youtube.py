62a3f141a12163560683c1f8b2e51c1f4d157f9b
Austin Monde <austinmonde7@gmail.com>
2024-05-12 15:43:44 +0200

    Completed project: Video downloader

--- youtubeDownloader/youtube.py
+++ youtubeDownloader/youtube.py
@@ -13,7 +13,25 @@ def download_video(url, save_path):
     except Exception as e:
         print(e)   
 
-url = "https://www.youtube.com/watch?v=ZZ6VhTBcc-c"
-save_path = "C:\Users\Nehan\OneDrive\Desktop\pythonProj"
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
 
-download_video(url, save_path)
+    return folder
+
+
+if __name__== "__main__":
+    root =tk.Tk()
+    root.withdraw()
+
+    video_url = input("Please enter a Youtube url: ")
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("Video download started....")
+        download_video(video_url, save_dir)
+        
+    else:
+        print("Invalid save location.")
+        
